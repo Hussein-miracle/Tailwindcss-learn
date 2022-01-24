cef47576948451f03576a35e2880f0f8f36d431d
Abdullahi Hussein <miraacle64@gmail.com>
2022-01-24 05:00:59 +0100

    feat:learning tailwindcss from netninja

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:"#ff6363",
+        secondary:{
+          100:"#e2e2d5",
+          200:"#888883"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
